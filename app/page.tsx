4570a28bec0ba10a733ae006cdc3232a8a9ca3bc
Tsaki <itspranav.pk@gmail.com>
2025-04-04 11:54:49 +0530

    tried to implement Image instead of img

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 import { Droplet, Search, Thermometer, Wind } from "lucide-react";
-import Image from "next/image";
 import { getWeatherdata } from "./action";
 import { WeatherData } from "@/types/weather";
 import { useState } from "react";
